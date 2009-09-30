cf85683ad068413d0039cd2edeac19e84c4d233b
Sid Wood <sidwood@me.com>
2009-09-30 12:24:16 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "Symlinking dotfiles to home directory"
+for name in *; do
+  target="$HOME/.$name"
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      echo "WARNING: $target exists but is not a symbolic link"
+    fi
+  else
+    skip=(bash install.sh README.md)
+    if [[ ! ${skip[*]} =~ "$name" ]]; then
+      echo "Creating $target"
+      ln -s "$PWD/$name" "$target"
+    fi
+  fi
+done
