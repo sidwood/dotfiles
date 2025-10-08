452b26dc9037daed3ac135fad43de4a3c2dc59bb
Sid Wood <sidwood@me.com>
2025-10-08 10:55:03 +0000

    stow: Add GNU Stow installation

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ abort() {
 command -v curl >/dev/null 2>&1 || abort 'curl required'
 command -v git >/dev/null 2>&1 || abort 'git required'
 command -v make >/dev/null 2>&1 || abort 'GNU Make required'
+command -v stow >/dev/null 2>&1 || abort 'GNU Stow required'
 
 #
 # Install Homebrew packages (macOS only)
@@ -41,23 +42,12 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 #
-# Symlink dotfiles to home directory
+# Stow dotfiles packages
 #
 
-echo "Symlinking dotfiles to home directory"
-for name in *; do
-  target="$HOME/.$name"
-  if [ -e "$target" ]; then
-    if [ ! -L "$target" ]; then
-      echo "WARNING: $target exists but is not a symbolic link"
-    fi
-  else
-    skip=(README.md bash install.sh tmp uninstall.sh zsh)
-    if [[ ! ${skip[*]} =~ "$name" ]]; then
-      echo "Creating $target"
-      ln -s "$PWD/$name" "$target"
-    fi
-  fi
+echo "Stowing dotfiles packages"
+for pkg in */; do
+  stow -v "${pkg%/}"
 done
 
 #
@@ -65,9 +55,9 @@ done
 #
 
 echo "Installing vcprompt"
-if [[ ! -x "bin/vcprompt" ]]; then
+if [[ ! -x "$HOME/.bin/vcprompt" ]]; then
   echo "Downloading vcprompt source"
-  rm bin/vcprompt 2>/dev/null
+  rm "$HOME/.bin/vcprompt" 2>/dev/null
   rm -rf tmp 2>/dev/null
   mkdir tmp 2>/dev/null
   cd tmp
@@ -79,7 +69,7 @@ if [[ ! -x "bin/vcprompt" ]]; then
   echo "Building vcprompt from source"
   cd tmp/vcprompt-$VCPROMPT_VERSION
   make
-  mv vcprompt ../../bin
+  mv vcprompt "$HOME/.bin/"
   cd -
   echo "Cleaning up"
   rm -rf tmp
--- uninstall.sh
+++ uninstall.sh
@@ -13,23 +13,15 @@ if [[ "$OSTYPE" == "darwin"* ]] && [ -f "$PWD/Brewfile" ]; then
   fi
 fi
 
-echo "Uninstalling dotfile symlinks"
-for name in *; do
-  target="$HOME/.$name"
-  if [ -e "$target" ]; then
-    if [ ! -L "$target" ]; then
-      echo "WARNING: $target exists but is not a symbolic link"
-    else
-      echo "Removing $target"
-      rm "$target"
-    fi
-  fi
+echo "Unstowing dotfiles packages"
+for pkg in */; do
+  stow -Dv "${pkg%/}"
 done
 
 echo "Uninstalling vcprompt"
-rm bin/vcprompt 2>/dev/null
+rm "$HOME/.bin/vcprompt" 2>/dev/null
 rm -rf tmp 2>/dev/null
 
 echo "Uninstalling vim-plug"
-rm -rf vim/autoload 2>/dev/null
-rm -rf vim/plugged 2>/dev/null
+rm -rf "$HOME/.vim/autoload" 2>/dev/null
+rm -rf "$HOME/.vim/plugged" 2>/dev/null
