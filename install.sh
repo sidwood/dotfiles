1128b8f88ef584385408de829d139fed659532bd
Sid Wood <sidwood@me.com>
2025-10-09 11:41:49 +0000

    vim: Use XDG Base Directory specification

--- install.sh
+++ install.sh
@@ -156,9 +156,9 @@ stow_dotfiles() {
 }
 
 install_vim_plugins() {
-  if [[ ! -d ~/.vim/autoload/plug.vim ]]; then
+  if [[ ! -d ~/.config/vim/autoload/plug.vim ]]; then
     echo "Installing vim-plug"
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    curl -fLo ~/.config/vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
 
