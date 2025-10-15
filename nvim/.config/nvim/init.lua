5b59efa2c4feafa195bcd7735b13876ae2c2d198
Sid Wood <sidwood@me.com>
2025-10-15 08:39:52 +0000

    nvim: Add base folding settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,5 +11,11 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
+-- Folding
+vim.opt.foldmethod = 'indent'
+vim.opt.foldnestmax = 10
+vim.opt.foldlevel = 2
+vim.opt.foldenable = false
+
 -- Appearance
 vim.opt.listchars:append({ tab = '▸ ', eol = '¬', trail = '·', nbsp = '⍽' })
