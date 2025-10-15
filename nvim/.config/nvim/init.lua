f742f5490b40ef81a2707d6a087dcb88044c454f
Sid Wood <sidwood@me.com>
2025-10-15 08:34:36 +0000

    nvim: Add init.lua (modernizing vim to nvim)
    Better late than never! ;o)

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,15 @@
+
+-- SETTINGS
+--------------------------------------------------------------------------------
+
+-- Set leader
+vim.g.mapleader= ","
+
+-- Whitespace
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- Appearance
+vim.opt.listchars:append({ tab = '▸ ', eol = '¬', trail = '·', nbsp = '⍽' })
