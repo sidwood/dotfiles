e95a04771cb4148ccb428c8f44c2353e4a1e37e0
Sid Wood <sidwood@me.com>
2025-10-17 14:11:38 +0000

    nvim: Add dashboard/greeter powered by alpha

---
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,18 @@
+return {
+  'goolord/alpha-nvim',
+  event = 'VimEnter',
+  config = function()
+    local dashboard = require('alpha.themes.dashboard')
+
+    dashboard.section.buttons.val = {
+      dashboard.button('f', '  Find file', ':Telescope find_files<CR>'),
+      dashboard.button('r', '  Recent files', ':Telescope oldfiles<CR>'),
+      dashboard.button('g', '  Find text', ':Telescope live_grep<CR>'),
+      dashboard.button('n', '  New file', ':ene <BAR> startinsert<CR>'),
+      dashboard.button('c', '  Configuration', ':e ~/.config/nvim/init.lua<CR>'),
+      dashboard.button('q', '  Quit', ':qa<CR>'),
+    }
+
+    require('alpha').setup(dashboard.config)
+  end,
+}
