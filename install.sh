d92a49333eb803b7325a3c899eeacea069f2a7f9
Sid Wood <sidwood@me.com>
2025-10-20 13:12:00 +0000

    install: Skip bash stow package on Omarchy
    Omarchy manages .bashrc

--- install.sh
+++ install.sh
@@ -185,6 +185,8 @@ stow_dotfiles() {
   echo "Symlinking dotfile packages"
   for pkg in */; do
     [[ "$pkg" == "macos/" || "$pkg" == "alfred/" ]] && continue
+    # Skip bash on Omarchy (it manages ~/.bashrc)
+    is_omarchy && [[ "$pkg" == "bash/" ]] && continue
     stow -v -t "$HOME" "${pkg%/}"
   done
 }
