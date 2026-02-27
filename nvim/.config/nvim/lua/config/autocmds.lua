719266835ccd0314ca017c27027f382a6c021fe4
Sid Wood <sidwood@me.com>
2026-02-27 19:33:48 +0000

    nvim: Update spell check highlights and status bar

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,61 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     vim.api.nvim_win_set_cursor(0, pos)
   end,
 })
+
+-- Make git commit subject length visually obvious:
+-- - first line chars 1-50 => green
+-- - first line chars 51+ => warning highlight
+local gitcommit_subject_ns = vim.api.nvim_create_namespace('GitCommitSubjectHighlight')
+
+local function highlight_gitcommit_subject(bufnr)
+  local line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
+  local line_len_bytes = #line
+  local line_len_chars = vim.fn.strchars(line)
+  local cutoff_byte = line_len_bytes
+
+  if line_len_chars > 50 then
+    cutoff_byte = vim.str_byteindex(line, 50)
+  end
+
+  vim.api.nvim_buf_clear_namespace(bufnr, gitcommit_subject_ns, 0, 1)
+
+  if line_len_bytes == 0 then
+    return
+  end
+
+  vim.api.nvim_buf_add_highlight(bufnr, gitcommit_subject_ns, 'GitCommitSubjectOk', 0, 0, cutoff_byte)
+
+  if line_len_bytes > cutoff_byte then
+    vim.api.nvim_buf_add_highlight(bufnr, gitcommit_subject_ns, 'GitCommitSubjectTooLong', 0, cutoff_byte, -1)
+  end
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'gitcommit',
+  callback = function(args)
+    vim.api.nvim_set_hl(0, 'GitCommitSubjectOk', {
+      fg = '#9acd32',
+      bold = true,
+      nocombine = true,
+    })
+    vim.api.nvim_set_hl(0, 'GitCommitSubjectTooLong', {
+      fg = '#ffffff',
+      bg = '#d70000',
+      bold = true,
+      nocombine = true,
+    })
+
+    local group = vim.api.nvim_create_augroup('GitCommitSubjectHighlight_' .. args.buf, { clear = true })
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'TextChanged', 'TextChangedI', 'InsertLeave' }, {
+      group = group,
+      buffer = args.buf,
+      callback = function()
+        highlight_gitcommit_subject(args.buf)
+      end,
+      desc = 'Highlight git commit subject length',
+    })
+
+    highlight_gitcommit_subject(args.buf)
+  end,
+  desc = 'Set up git commit subject highlighting',
+})
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,8 +3,47 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    local function apply_spell_highlights()
+      -- Use block-style spell highlights to avoid visual clashes with markdown underline syntax.
+      vim.api.nvim_set_hl(0, 'SpellBad', {
+        underline = false,
+        undercurl = false,
+        bg = '#5f0000',
+        fg = '#ffd7d7',
+        nocombine = true,
+      })
+      vim.api.nvim_set_hl(0, 'SpellCap', {
+        underline = false,
+        undercurl = false,
+        bg = '#5f5f00',
+        fg = '#ffffd7',
+        nocombine = true,
+      })
+      vim.api.nvim_set_hl(0, 'SpellRare', {
+        underline = false,
+        undercurl = false,
+        bg = '#5f3f00',
+        fg = '#ffe7c7',
+        nocombine = true,
+      })
+      vim.api.nvim_set_hl(0, 'SpellLocal', {
+        underline = false,
+        undercurl = false,
+        bg = '#5f3f00',
+        fg = '#ffe7c7',
+        nocombine = true,
+      })
+    end
+
     vim.o.background = 'dark'
     require('solarized').setup()
     vim.cmd.colorscheme('solarized')
+
+    apply_spell_highlights()
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      group = vim.api.nvim_create_augroup('CustomSpellHighlights', { clear = true }),
+      callback = apply_spell_highlights,
+      desc = 'Restore custom spell highlight colors',
+    })
   end,
 }
